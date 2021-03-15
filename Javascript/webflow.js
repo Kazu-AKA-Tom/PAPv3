f5dc7a0d69c6cc9f778124689794165d0d188b98
Kazu_AKA_Tom <tomeoliveirareis03@gmail.com>
2021-03-15 16:53:32 +0000

    Deletion of unecessary code

--- Javascript/webflow.js
+++ Javascript/webflow.js
@@ -9,22 +9,3 @@ $('.navTrigger').click(function () {
   $("#mainListDiv").fadeIn();
 
 });
-
-const buttons = document.querySelectorAll('.project');
-const overlay = document.querySelector('.overlay');
-const overlayImage = document.querySelector('.overlay__inner img');
-
-function open(e) {
-  overlay.classList.add('open');
-  const src= e.currentTarget.querySelector('img').src;
-  overlayImage.src = src;
-}
-
-function close() {
-  overlay.classList.remove('open');
-}
-
-buttons.forEach(button => button.addEventListener('click', open));
-overlay.addEventListener('click', close);
-
-

a78f471d38f15e2f1057f86f7a71857a89c084fe
Kazu_AKA_Tom <tomeoliveirareis03@gmail.com>
2021-04-09 14:43:44 +0100

    Deleted code

--- Javascript/webflow.js
+++ Javascript/webflow.js
@@ -9,27 +9,3 @@ $('.navTrigger').click(function () {
   $("#mainListDiv").fadeIn();
 
 });
-
-const images = document.querySelectorAll('.img-gallery');
-const images-light = document.querySelector('.add-image');
-const container-light = document.querySelector('.image-light');
-
-imagenes.forEach(imagen =>{
-    imagen.addEventListener('click', ()=>{
-        aparecerImagen(imagen.getAttribute('src'))
-    
-    })
-})
-
-container-light.addEventListener('click', (e) =>{
-    if(e.target !== imagenesLight){
-        container-light.classList.remove('show');
-        image-light.classList.remove('showImage'); 
-    }
-})
-
-const aparecerImagen = (imagen) =>{
-    imagenesLight.src = imagen;
-    contenedorLight.classList.add('show');
-    imagenesLight.classList.add('showImage');
-}
